bb9ada9312c5050315c8fc5f87887954768c1c5d
Yvan <yvan.zy@gmail.com>
2018-08-17 17:02:48 +0800

    feat:add autocomplete feature

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp init_vimrc  ~/.vimrc
+pip install pep8 flake8 pyflakes yapf
 vim +BundleInstall +BundleClean! +qa
