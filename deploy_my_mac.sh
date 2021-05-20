c627913f57fb6e21d39cc840f5c810630ed97843
Yvan <yvan.zy@gmail.com>
2021-05-20 11:09:12 +0800

    feat: add a theme for iterm2

--- deploy_my_mac.sh
+++ deploy_my_mac.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
-#Change the shell
+# Change the shell
 chsh -s "$(command -v zsh)"
 
-#Install the xcode command line tools
+# Install the xcode command line tools
 xcode-select --install
 
-#Install the pip
+# Install the pip
 # easy_install --user pip
 
-#Install Oh-My-Zsh
+# Install Oh-My-Zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-#Install Powerline
+# Install Powerline
 # pip install --user powerline-status
 
-#Install the fonts of the Powerline
+# Install the fonts of the Powerline
 # git clone https://github.com/powerline/fonts.git
 # sh fonts/install.sh
 
-#Install agnoster theme
+# Install agnoster theme
 # git clone https://github.com/fcamblor/oh-my-zsh-agnoster-fcamblor.git
 # sh oh-my-zsh-agnoster-fcamblor/install
 
-#Add the highlighting effect
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/
+# Add the highlighting effect
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Install iTerm2 theme dracula
+mkdir -p ~/Code/github && git clone https://github.com/dracula/iterm.git ~/Code/github/iterm
 
 # 此时ls -al查看文件夹显示无色。需要修改iTerm2: Preferences - Profiles - Text - Text Rendering，将 Draw bold text in bright colors 前面的勾去掉
 
-#Install Homebrew
+# Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install nvs
